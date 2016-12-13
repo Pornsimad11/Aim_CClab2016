5ab7b1def969faf730f4bcc5102ea706044a975a
aim <aim@aim.wireless.newschool.edu>
2016-12-13 17:40:11 -0500

    finals update

---
+++ w15_w15_FINALS/Survival of the Baby Iguana/Assets/Scripts/GameManager.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Audio; 
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject playerPrefab; 
+	public Text continueText;
+	public Text scoreText; 
+	public Text titleText;
+	public AudioSource runningMusic; 
+	public AudioSource screamSound; 
+
+	private float timeElapsed = 0f;
+	private float bestTime = 0f;
+	private float blinkTime = 0f; 
+	private bool blink; 
+	private bool gameStarted; 
+	private TimeManager timeManager; 
+	private GameObject player; 
+	private GameObject floor; 
+	private Spawner spawner;
+	private bool beatBestTime;
+
+	void Awake(){
+		floor = GameObject.Find ("Ground");
+		spawner = GameObject.Find ("Spawner").GetComponent<Spawner> ();
+		timeManager = GetComponent <TimeManager> (); 
+	}
+
+	// Use this for initialization
+	void Start () {
+		var floorHeight = floor.transform.localScale.y;
+		var pos = floor.transform.position; 
+		pos.x = 0;
+		pos.y = -((Screen.height / PixelPerfectCamera.pixelsToUnits) / 2) + (floorHeight / 2); 
+		floor.transform.position = pos; 
+
+		screamSound.Pause();
+		runningMusic.Pause();
+		spawner.active = false; 
+		Time.timeScale = 0; 
+
+		continueText.text = "Press any button to run for your life!";
+		titleText.text = "Survival of the Baby Iguana";
+		bestTime = PlayerPrefs.GetFloat ("BestTime"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!gameStarted && Time.timeScale == 0) { 
+
+			if (Input.anyKeyDown) { 
+
+				timeManager.ManipulateTime (1, 1f); 
+				ResetGame (); 
+
+			}
+		}
+
+		if (!gameStarted) { 
+			blinkTime++; 
+
+			if (blinkTime % 20 == 0) { 
+				blink = !blink; 
+			} 
+
+			continueText.canvasRenderer.SetAlpha (blink ? 0 : 3); 
+
+			titleText.canvasRenderer.SetAlpha (1);
+
+			var textColor = beatBestTime ? "DF82E1FF" : "#F17800FF"; 
+
+			scoreText.text = "Time: " + FormatTime (timeElapsed) + "\n<color="+textColor+">Best: " + FormatTime (bestTime)+"</color>"; 
+		}else{ 
+			timeElapsed += Time.deltaTime;
+			scoreText.text = "Time: "+FormatTime(timeElapsed);
+		}
+	}
+
+	void OnPlayerKilled(){ 
+
+		screamSound.Play();
+		spawner.active = false; 
+		runningMusic.Pause();
+		var playerDestroyScript = player.GetComponent<DestroyOffscreen> (); 
+		playerDestroyScript.DestroyCallback -= OnPlayerKilled;
+
+		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero; 
+		timeManager.ManipulateTime (0, 5.5f); 
+		gameStarted = false; 
+
+		titleText.text = "";
+		continueText.text = "You're not completely dead yet, run again!";
+
+		if (timeElapsed > bestTime) { 
+			bestTime = timeElapsed; 
+			PlayerPrefs.SetFloat("BestTime", bestTime); 
+			beatBestTime = true; 
+		}
+
+	}
+
+	void ResetGame() { 
+		screamSound.Pause();
+		spawner.active = true; 
+		runningMusic.Play();
+		player = GameObjectUtil.Instantiate (playerPrefab, new Vector3 (0, (Screen.height / PixelPerfectCamera.pixelsToUnits) / 1.7f + 100, 0)); 
+
+		var playerDestroyScript = player.GetComponent<DestroyOffscreen> (); 
+		playerDestroyScript.DestroyCallback += OnPlayerKilled;
+
+		gameStarted = true; 
+
+		continueText.canvasRenderer.SetAlpha (0);
+		titleText.canvasRenderer.SetAlpha (0);
+
+		timeElapsed = 0; 
+		beatBestTime = false;
+	
+	}
+
+	string FormatTime(float value){ 
+		TimeSpan t = TimeSpan.FromSeconds (value); 
+
+		return string.Format("{0:D2}:{1:D2}",t.Minutes, t.Seconds); 
+	}
+}
---
+++ w15_w15_FINALS/Survival of the Baby Iguana/README.md
@@ -0,0 +1,35 @@
+## Concept
+
+The Survival of the Baby Iguana 
+
+I was inspired by the Iguana vs Snakes - Planet Earth II clip(https://www.youtube.com/watch?v=Rv9hn4IGofM) in how the iguana has strive to survive. It reminded me to push forward in my finals week and so I want to dedicate the game to it. 
+
+https://www.youtube.com/watch?v=0h2T98RVmKc&feature=youtu.be
+
+## What I did 
+
+I created my assets and followed Jesse Freeman 2D platformer game tutorial for the main parts of the game. I created the infinite scrolling background, put in the sounds, and designed the game so that the snake will atttack more violently like the real clip. 
+
+## Credits 
+
+My game is based on 
+
+Unity 5 2D Essential Training (2015) by Jesse Freeman
+(https://www.lynda.com/Unity-2D-tutorials/Welcome/185319/386632-4.html?srchtrk=index%3a1%0alinktypeid%3a2%0aq%3aunity+2d+platformer+%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2)
+
+To create 2D Infinite Scrolling Background the Simplest Way I used 
+(https://www.youtube.com/watch?v=HrDxnMI7pCc)
+
+For the iguana run sprite I used (http://capnani.blogspot.com/p/applied-animation.html) as a study of how an iguana should run
+
+For the snake sprite I used http://spritedatabase.net/file/9053 and (http://spritedatabase.net/file/6556 as a study of how the snakes should move)
+
+For the background sound I used ♩♫ Epic Chase Music ♪♬ - Run (Copyright and Royalty Free) (https://www.youtube.com/watch?v=ujsCRw-eA0o)
+
+For the scream I used Wilhelm Scream sound effect (https://www.youtube.com/watch?v=9FHw2aItRlw)
+
+## What did I learn
+
+I learned the basic aspect of how to create a game on unity, how to create a spawner, and how to manage the game. 
+
+
